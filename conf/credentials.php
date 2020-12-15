ff5f580e6f02ef8687aa7f210604856fc1525bbd
Daniele D'Agostino <principelele@gmail.com>
2020-12-15 09:39:57 +0100

    database credential updated

--- conf/credentials.php
+++ conf/credentials.php
@@ -1,7 +1,7 @@
 <?php
 	$config['displayErrorDetails'] = true;
-	$config['db']['host']   = "localhost";
-	$config['db']['user']   = "root";
-	$config['db']['pass']   = "";
-	$config['db']['dbname'] = "my_montanaridiricotta";
+	$config['db']['host']   = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+	$config['db']['user']   = "u6cswd4enkr1mrhp";
+	$config['db']['pass']   = "fcn0s93s0qub8rbk";
+	$config['db']['dbname'] = "ax52m07alhrlfni4";
 ?>
